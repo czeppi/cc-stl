cdf6d44a2c8ff6272c9063c499242301848f6c4a
Christian Czepluch <git@czeppi.de>
2024-11-05 06:13:49 +0100

    some refactorings swap y with z coordinates

--- show-stl.py
+++ show-stl.py
@@ -2,7 +2,7 @@ import sys
 import trimesh
 import numpy as np
 from PySide6.QtCore import Qt, QPoint
-from PySide6.QtGui import QMatrix4x4, QWindow
+from PySide6.QtGui import QMatrix4x4, QWindow, QVector3D
 from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtOpenGLWidgets import QOpenGLWidget
 from PySide6.QtOpenGL import QOpenGLShaderProgram, QOpenGLBuffer, QOpenGLVertexArrayObject, QOpenGLShader
@@ -21,11 +21,11 @@ class OpenGlWin(QOpenGLWidget):
         self._vao = None
 
         # Kamera-Parameter
-        self.camera_distance = 50.0  # Anfangs-Zoom-Distanz
-        self.camera_azimuth = 0.0  # Winkel um die y-Achse (horizontaler Winkel)
-        self.camera_elevation = 0.0  # Winkel um die x-Achse (vertikaler Winkel)
-        self.last_mouse_position = QPoint()
-        self.is_right_button_pressed = False
+        self._camera_distance = 70.0  # start zoom distance
+        self._camera_azimuth = 0.0  # angle around y axis (horizontal angle)
+        self._camera_elevation = 0.0  # angle around x-axis (vertical angle)
+        self._last_mouse_position = QPoint()
+        self._is_right_button_pressed = False
 
     def initializeGL(self):
         glEnable(GL_DEPTH_TEST)
@@ -33,6 +33,7 @@ class OpenGlWin(QOpenGLWidget):
         # read STL file
         self._mesh = trimesh.load(self._stl_path)
         vertices = np.array(self._mesh.vertices, dtype=np.float32)
+        vertices[:, [1, 2]] = vertices[:, [2, 1]]   # swap y and z, cause z should vertical axis
         faces = np.array(self._mesh.faces, dtype=np.uint32)
 
         # create shader-program
@@ -50,10 +51,51 @@ class OpenGlWin(QOpenGLWidget):
         fragment_src = """
             #version 330 core
             out vec4 fragColor;
+            
+            uniform vec3 ambientColor;    // Umgebungslichtfarbe
+            uniform vec3 lightPosition;   // position of the light source
+            uniform vec3 lightColor;      // color of light source
+            uniform vec3 objectColor; 
+            
             void main() {
-                fragColor = vec4(0.6, 0.6, 0.8, 1.0);
+                vec3 ambient = ambientColor;
+                
+                // Diffusanteil basierend auf Lichtposition (vereinfachtes Lichtmodell)
+                vec3 lightDir = normalize(lightPosition - vec3(0.0, 0.0, 0.0)); // Licht in Richtung Ursprung
+                float diff = max(dot(vec3(0.0, 0.0, 1.0), lightDir), 0.0);  // Diffuslicht entlang der Normalen
+                vec3 diffuse = diff * lightColor;
+                
+                vec3 result = (ambient + diffuse) * objectColor;
+                
+                // fragColor = vec4(0.6, 0.6, 0.8, 1.0);
+                fragColor = vec4(result, 1.0);
             }
         """
+
+        # fragment_src = """
+        #     #version 330 core
+        #     out vec4 fragColor;
+        #
+        #     // Konstanten für Beleuchtung
+        #     uniform vec3 ambientColor;    // Umgebungslichtfarbe
+        #     uniform vec3 lightPosition;   // Position der Lichtquelle
+        #     uniform vec3 lightColor;      // Farbe des Lichts
+        #     uniform vec3 objectColor;     // Farbe des Objekts
+        #
+        #     void main() {
+        #         // Umgebungslichtanteil
+        #         vec3 ambient = ambientColor;
+        #
+        #         // Diffusanteil basierend auf Lichtposition (vereinfachtes Lichtmodell)
+        #         vec3 lightDir = normalize(lightPosition - vec3(0.0, 0.0, 0.0)); // Licht in Richtung Ursprung
+        #         float diff = max(dot(vec3(0.0, 0.0, 1.0), lightDir), 0.0);  // Diffuslicht entlang der Normalen
+        #         vec3 diffuse = diff * lightColor;
+        #
+        #         // Kombination von Umgebungs- und Diffuslicht
+        #         vec3 result = (ambient + diffuse) * objectColor;
+        #         //fragColor = vec4(result, 1.0);
+        #         fragColor = vec4(0.6, 0.6, 0.8, 1.0);
+        # """
         self._shader_program = QOpenGLShaderProgram(self)
         self._shader_program.addShaderFromSourceCode(QOpenGLShader.Vertex, vertex_src)
         self._shader_program.addShaderFromSourceCode(QOpenGLShader.Fragment, fragment_src)
@@ -84,6 +126,12 @@ class OpenGlWin(QOpenGLWidget):
         self._shader_program.enableAttributeArray(pos_location)
         glVertexAttribPointer(pos_location, 3, GL_FLOAT, GL_FALSE, 0, None)
 
+        # set light parameters
+        self._shader_program.setUniformValue("ambientColor", QVector3D(0.2, 0.2, 0.2))  # Gedimmtes Umgebungslicht
+        self._shader_program.setUniformValue("lightPosition", QVector3D(100.0, 100.0, 100.0))  # position of the light source
+        self._shader_program.setUniformValue("lightColor", QVector3D(0.8, 0.8, 0.8))  # white light
+        self._shader_program.setUniformValue("objectColor", QVector3D(0.6, 0.6, 0.8))  # object color
+
         self._vao.release()
         self._vbo.release()
         self._ebo.release()
@@ -106,11 +154,8 @@ class OpenGlWin(QOpenGLWidget):
         self._vao.bind()
 
         # use transformation
-        #model_matrix = self._create_translation_matrix(0, 0, -30)
         model_matrix = self._create_eye_matrix()
 
-        # view_matrix = np.eye(4, dtype=np.float32)
-        # view_matrix = self._create_eye_matrix()
         view_matrix = self._calculate_view_matrix()
 
         self._shader_program.setUniformValue("model", model_matrix)
@@ -131,15 +176,6 @@ class OpenGlWin(QOpenGLWidget):
         )
 
     @staticmethod
-    def _create_eye_matrix() -> QMatrix4x4:
-        return QMatrix4x4(
-            1, 0, 0, 0,
-            0, 1, 0, 0,
-            0, 0, 1, 0,
-            0, 0, 0, 1
-        )
-
-    @staticmethod
     def _create_perspective_matrix(fov, aspect, near, far) -> QMatrix4x4:
         f = 1.0 / np.tan(np.radians(fov) / 2)
         return QMatrix4x4(
@@ -148,12 +184,6 @@ class OpenGlWin(QOpenGLWidget):
             0, 0, (far + near) / (near - far), (2 * far * near) / (near - far),
             0, 0, -1, 0
         )
-        # return np.array([
-        #     [f / aspect, 0, 0, 0],
-        #     [0, f, 0, 0],
-        #     [0, 0, (far + near) / (near - far), (2 * far * near) / (near - far)],
-        #     [0, 0, -1, 0]
-        # ], dtype=np.float32)
 
     @staticmethod
     def _create_translation_matrix(x, y, z) -> QMatrix4x4:
@@ -163,20 +193,14 @@ class OpenGlWin(QOpenGLWidget):
             0, 0, 1, z,
             0, 0, 0, 1
         )
-        # return np.array([
-        #     [1, 0, 0, x],
-        #     [0, 1, 0, y],
-        #     [0, 0, 1, z],
-        #     [0, 0, 0, 1]
-        # ], dtype=np.float32)
 
     def _calculate_view_matrix(self) -> QMatrix4x4:
         """ cals view matrix in dependency of the camera
         """
         # calc camera position with azimuth and elevation
-        x = self.camera_distance * np.cos(np.radians(self.camera_elevation)) * np.sin(np.radians(self.camera_azimuth))
-        y = self.camera_distance * np.sin(np.radians(self.camera_elevation))
-        z = self.camera_distance * np.cos(np.radians(self.camera_elevation)) * np.cos(np.radians(self.camera_azimuth))
+        x = self._camera_distance * np.cos(np.radians(self._camera_elevation)) * np.sin(np.radians(self._camera_azimuth))
+        y = self._camera_distance * np.sin(np.radians(self._camera_elevation))
+        z = self._camera_distance * np.cos(np.radians(self._camera_elevation)) * np.cos(np.radians(self._camera_azimuth))
 
         # camera view point and "up"-vector
         eye = np.array([x, y, z], dtype=np.float32)
@@ -190,15 +214,6 @@ class OpenGlWin(QOpenGLWidget):
         s /= np.linalg.norm(s)
         u = np.cross(f, s)
 
-        # view_matrix = np.eye(4, dtype=np.float32)
-        # view_matrix[:3, 0] = s
-        # view_matrix[:3, 1] = u
-        # view_matrix[:3, 2] = -f
-        # view_matrix[0, 3] = -np.dot(s, eye)
-        # view_matrix[1, 3] = -np.dot(u, eye)
-        # view_matrix[2, 3] = np.dot(f, eye)
-        # return view_matrix
-
         return QMatrix4x4(
             s[0], u[0], f[0], -np.dot(s, eye),
             s[1], u[1], f[1], -np.dot(u, eye),
@@ -208,30 +223,30 @@ class OpenGlWin(QOpenGLWidget):
 
     def mousePressEvent(self, event):
         if event.button() == Qt.RightButton:
-            self.is_right_button_pressed = True
-            self.last_mouse_position = event.position().toPoint()
+            self._is_right_button_pressed = True
+            self._last_mouse_position = event.position().toPoint()
 
     def mouseReleaseEvent(self, event):
         if event.button() == Qt.RightButton:
-            self.is_right_button_pressed = False
+            self._is_right_button_pressed = False
 
     def mouseMoveEvent(self, event):
-        if self.is_right_button_pressed:
-            delta = event.position().toPoint() - self.last_mouse_position
-            self.camera_azimuth += delta.x() * 0.5  # Sensitivität für Azimuth
-            self.camera_elevation += delta.y() * 0.5  # Sensitivität für Elevation
+        if self._is_right_button_pressed:
+            delta = event.position().toPoint() - self._last_mouse_position
+            self._camera_azimuth += delta.x() * 0.5  # Sensitivität für Azimuth
+            self._camera_elevation += delta.y() * 0.5  # Sensitivität für Elevation
 
             # Begrenze Elevation, damit der Horizont immer gerade bleibt
-            self.camera_elevation = max(-89.0, min(89.0, self.camera_elevation))
+            self._camera_elevation = max(-89.0, min(89.0, self._camera_elevation))
 
-            self.last_mouse_position = event.position().toPoint()
+            self._last_mouse_position = event.position().toPoint()
             self.update()  # Bildschirm aktualisieren
 
     def wheelEvent(self, event):
         # Zoom-Faktor anpassen (z.B., 1.1 für schnelles Zoomen oder 1.05 für langsameres Zoomen)
         zoom_factor = 0.9 if event.angleDelta().y() > 0 else 1.1
-        self.camera_distance *= zoom_factor
-        self.camera_distance = max(1.0, min(100.0, self.camera_distance))  # Begrenze den Zoombereich
+        self._camera_distance *= zoom_factor
+        self._camera_distance = max(1.0, min(100.0, self._camera_distance))  # Begrenze den Zoombereich
         self.update()  # Bildschirm aktualisieren
 
 
