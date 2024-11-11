cddb6b40535d72ad537e875c20ac8941bd69e7dd
Christian Czepluch <git@czeppi.de>
2024-11-11 17:16:57 +0100

    create splitter

--- mainwindow.py
+++ mainwindow.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import sys
 
 import trimesh
-from PySide6.QtWidgets import QMainWindow, QApplication
+from PySide6.QtWidgets import QMainWindow, QApplication, QSplitter, QLabel
 
 from openglwidget import GL_VIEW_SIZE, OpenGlWin
 
@@ -18,18 +18,36 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("STL Example")
-        #self.setFixedSize(*GL_VIEW_SIZE)
         self.resize(*GL_VIEW_SIZE)
 
         self._mesh = self._read_mesh(STL_PATH)
-        self._open_gl_widget = OpenGlWin(mesh= self._mesh, parent=self)
-        self.setCentralWidget(self._open_gl_widget)
+
+        self._splitter = MainSplitter(self._mesh)
+        self.setCentralWidget(self._splitter)
 
     @staticmethod
     def _read_mesh(stl_path: str) -> trimesh.Trimesh:
         mesh = trimesh.load(stl_path)
         #self._rotate_mesh_90degree_around_x_axis(mesh)
         return mesh
+
+
+class MainSplitter(QSplitter):
+
+    def __init__(self, mesh: trimesh.Trimesh):
+        super().__init__()
+
+        self._mesh = mesh
+
+        self._open_gl_widget = OpenGlWin(mesh= self._mesh, parent=self)
+        self._label_widget = QLabel(parent=self)
+        self._label_widget.setText('hallo')
+
+        self.addWidget(self._open_gl_widget)
+        self.addWidget(self._label_widget)
+
+        self.setStretchFactor(0, 1)
+        self.setStretchFactor(1, 0)
 
 
 if __name__ == "__main__":
