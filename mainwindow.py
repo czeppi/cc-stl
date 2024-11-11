d5ee8d5d33ecfdedd023c57b59100b16ce739a98
Christian Czepluch <git@czeppi.de>
2024-11-11 17:26:00 +0100

    add first dummy menu

--- mainwindow.py
+++ mainwindow.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
 import sys
+from typing import Callable
 
 import trimesh
-from PySide6.QtWidgets import QMainWindow, QApplication, QSplitter, QLabel
+from PySide6.QtGui import QAction
+from PySide6.QtWidgets import QMainWindow, QApplication, QSplitter, QLabel, QFileDialog
 
 from openglwidget import GL_VIEW_SIZE, OpenGlWin
 
@@ -22,14 +24,36 @@ class MainWindow(QMainWindow):
 
         self._mesh = self._read_mesh(STL_PATH)
 
+        self._add_menubar()
+
         self._splitter = MainSplitter(self._mesh)
         self.setCentralWidget(self._splitter)
+
+    def _add_menubar(self):
+        menu_bar = self.menuBar()
+
+        file_menu = menu_bar.addMenu('File')
+        file_menu.addAction(self._create_action('open...', self.on_file_open))
+
+    def _create_action(self, name: str, handler: Callable[[], None]) -> QAction:
+        action = QAction(name, self)
+        action.triggered.connect(handler)
+        return action
 
     @staticmethod
     def _read_mesh(stl_path: str) -> trimesh.Trimesh:
         mesh = trimesh.load(stl_path)
         #self._rotate_mesh_90degree_around_x_axis(mesh)
         return mesh
+
+    def on_file_open(self) -> None:
+        file_dlg = QFileDialog(self)
+        file_dlg.setAcceptMode(QFileDialog.AcceptMode.AcceptOpen)
+        file_dlg.setWindowTitle('Open File')
+
+        ok = file_dlg.exec()
+        if not ok:
+            return
 
 
 class MainSplitter(QSplitter):
